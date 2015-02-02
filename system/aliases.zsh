ea8ac2ff9f82190ca2fff38a443f18a8767758a8
Levi Thomason <me@levithomason.com>
2015-02-02 09:47:47 -0800

    adds bulk rename "ren" command

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -11,4 +11,28 @@ fi
 
 alias odot="open ~/.dotfiles"
 alias "cd.."="cd .."
+alias ren=fnRename
 alias src="cd ~/src/"
+
+# Bulk Rename - Renames all occurrences in the current directory
+#
+# Given:
+#   foo-thing.txt
+#   another-foo-item.js
+#   foo/
+#     last-foo.html
+#
+# Command:
+#   ren foo bar
+#
+# Results:
+#   bar-thing.txt
+#   another-bar-item.js
+#   bar/
+#     last-bar.html
+
+fnRename() {
+  for a in *$1*; do
+    mv $a ${a//$1/$2};
+  done;
+}
