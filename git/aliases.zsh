e011043001edf877f625a66021b7ced0362c5709
Levi Thomason <me@levithomason.com>
2015-03-10 09:47:23 -0700

    adds go list with selection

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -29,194 +29,229 @@ alias gt='git stash'
 alias gta='git stash apply'
 
 fnGitAdd() {
-    git add -A .
+  git add -A .
 }
 
 # Does a hard reset with double confirmation if there are uncommitted changes.
 fnGitReset() {
-    uncommitted_changes=($(git status -s))
+  uncommitted_changes=($(git status -s))
 
-    if (( ${#uncommitted_changes[@]} == 0 )) then
-        git reset --hard
-    else
-        echo ""
-        echo "Uncommited chagnes:"
-        git status -s
-        
-        echo ""
-        read -q "CONFIRM?PERMANENTLY loose uncommitted changes above? (y/N) "
-
-        if [[ $CONFIRM == "y" ]] then
-            echo ""
-            read -q "CONFIRM_AGAIN?You're aware there exists no black magic that can bring these back? (y/N) "
-            echo ""
-      
-            if [[ $CONFIRM_AGAIN == "y" ]] then
-                git reset --hard
-            fi
-        fi
-    fi
+  if (( ${#uncommitted_changes[@]} == 0 )) then
+    git reset --hard
+  else
+    echo ""
+    echo "Uncommited chagnes:"
+    git status -s
     
-    unset uncommitted_changes
-    unset CONFIRM
-    unset CONFIRM_AGAIN
+    echo ""
+    read -q "CONFIRM?PERMANENTLY loose uncommitted changes above? (y/N) "
+
+    if [[ $CONFIRM == "y" ]] then
+      echo ""
+      read -q "CONFIRM_AGAIN?You're aware there exists no black magic that can bring these back? (y/N) "
+      echo ""
+    
+      if [[ $CONFIRM_AGAIN == "y" ]] then
+        git reset --hard
+      fi
+    fi
+  fi
+  
+  unset uncommitted_changes
+  unset CONFIRM
+  unset CONFIRM_AGAIN
 }
 
 fnGitPrune() {
-    # save current branch
-    original_branch=$(git branch | grep "* ");
-    original_branch=${original_branch/"* "};
+  # save current branch
+  original_branch=$(git branch | grep "* ");
+  original_branch=${original_branch/"* "};
 
-    git checkout master
+  git checkout master
 
-    # trim fetched to match remotes
-    git pull --prune
+  # trim fetched to match remotes
+  git pull --prune
 
-    #                    +merged branches      -current       -specific
-    branches_to_delete=$(git branch --merged | grep -v "\*" | egrep -v "master")
+  #                    +merged branches      -current       -specific
+  branches_to_delete=$(git branch --merged | grep -v "\*" | egrep -v "master")
 
-    # array from lines
-    branches_to_delete=("${(f)branches_to_delete}")
+  # array from lines
+  branches_to_delete=("${(f)branches_to_delete}")
 
-    echo "\nLocal branches already merged:"
-    # list branches
-    for branch in $branches_to_delete; do
-        echo "$branch"
-    done
-    
-    echo ""
-    read -q "CONFIRM?Delete ALL these? (y/N) "
-    echo ""
+  echo "\nLocal branches already merged:"
+  # list branches
+  for branch in $branches_to_delete; do
+    echo "$branch"
+  done
+  
+  echo ""
+  read -q "CONFIRM?Delete ALL these? (y/N) "
+  echo ""
 
-    if [[ $CONFIRM == "y" ]]
-        then
-            # delete branches
-            for branch in $branches_to_delete; do
-                git branch -d ${branch// /}
-            done
-            
-        else
-            echo "\nCancelled"
-    fi
+  if [[ $CONFIRM == "y" ]]
+    then
+      # delete branches
+      for branch in $branches_to_delete; do
+        git branch -d ${branch// /}
+      done
+      
+    else
+      echo "\nCancelled"
+  fi
 
-    git checkout $original_branch
+  git checkout $original_branch
 
-    fnGitBranch
-    
-    unset branches_to_delete
-    unset original_branch
+  fnGitBranch
+  
+  unset branches_to_delete
+  unset original_branch
 }
 
 fnGitBranch() {
-    if (( $# == 0 ))
-    then
-        git branch -a
-    else
-        git pull
-        git branch feature/$1
-        git checkout feature/$1
-        git push -u origin feature/$1
-    fi
+  if (( $# == 0 )) then
+    git branch -a
+  else
+    git pull
+    git branch feature/$1
+    git checkout feature/$1
+    git push -u origin feature/$1
+  fi
 }
 
 fnGitDelete() {
-    if (( $# == 0 ))
-    then
-        echo "\n    Usage: gd <branch-to-delete> ..."
-    else
-        git branch -D $*
-        fnGitBranch
-    fi
+  if (( $# == 0 )) then
+    echo "\n  Usage: gd <branch-to-delete> ..."
+  else
+    git branch -D $*
+    fnGitBranch
+  fi
 }
 
 fnGitDeleteFeature() {
-    if (( $# == 0 ))
-    then
-        echo "\n    Usage: gd <branch-to-delete> ..."
-    else
-        git branch -D feature/$*
-        fnGitBranch
-    fi
+  if (( $# == 0 )) then
+    echo "\n  Usage: gd <branch-to-delete> ..."
+  else
+    git branch -D feature/$*
+    fnGitBranch
+  fi
 }
 
 fnGitBranchMaster() {
-    if (( $# == 0 ))
-    then
-        git branch -a
-    else
-        git checkout master
-        git pull
-        git branch feature/$1
-        git checkout feature/$1
-        git push -u origin feature/$1
-    fi
+  if (( $# == 0 )) then
+    git branch -a
+  else
+    git checkout master
+    git pull
+    git branch feature/$1
+    git checkout feature/$1
+    git push -u origin feature/$1
+  fi
 }
 
 fnGitCheckout() {
-    if (( $# == 0 ))
-    then
-        git checkout master
+  if (( $# > 0 )) then
+    git checkout $*
+  else
+    go_counter=1
+    go_branches=()
+  
+    # get branches, excluding current and master
+    go_branches=$(git branch | grep -v "\*" | egrep -v "master")
+  
+    # array from lines
+    go_branches=("${(f)go_branches}")
+  
+    for go_branch in $go_branches; do
+      # replace spaces
+      go_branch=${go_branch//" "}
+      
+      # add branch to array
+      go_branches[$go_counter]=$go_branch
+      
+      # print array index and folder name
+      echo "$go_counter: $go_branch"
+  
+      # increment counter
+      go_counter=$((go_counter+1))
+    done;
+    
+    echo ""
+    read "go_choice?master/#: "
+    
+    # checkout master by default
+    if [[ $go_choice == "" ]] then
+      git checkout master
     else
-        git checkout feature/$1
+      go_branch_name=$go_branches[$go_choice]
+    
+      # checkout branch if a valid array item was selected
+      if [[ $go_branch_name != "" ]] then
+        git checkout $go_branch_name
+      else
+        fnGitCheckout $1
+      fi
     fi
+  fi
+
+  unset go_counter
+  unset go_branches
+  unset go_branch
+  unset go_choice
+  unset go_branch_name
 }
 
 fnGitCheckoutPull() {
-    fnGitCheckout $1
-    git pull
+  fnGitCheckout $1
+  git pull
 }
 
 fnGitCommit() {
-    fnGitAdd
-    git commit -m "$1"
+  fnGitAdd
+  git commit -m "$1"
 }
 
 fnGitCommitPush() {
-    if (( $# == 0 ))
-    then
-        echo "commit what sucka?!"
-    else
-      fnGitCommit $1
-      git pull
-      git push
-    fi
+  if (( $# == 0 )) then
+    echo "commit what sucka?!"
+  else
+    fnGitCommit $1
+    git pull
+    git push
+  fi
 }
 
 fnGitMerge() {
-    if (( $# == 0 ))
-    then
-        # save current branch
-        original_branch=$(git branch | grep "* ");
-        original_branch=${original_branch/"* "};
+  if (( $# == 0 )) then
+    # save current branch
+    original_branch=$(git branch | grep "* ");
+    original_branch=${original_branch/"* "};
+  
+    # update master
+    git checkout master
+    git pull
     
-        # update master
-        git checkout master
-        git pull
-        
-        # merge into original branch
-        git checkout $original_branch
-        git merge master
+    # merge into original branch
+    git checkout $original_branch
+    git merge master
 
-        unset original_branch
-    else
-        git merge $1
-    fi
+    unset original_branch
+  else
+    git merge $1
+  fi
 }
 
 fnGitLog() {
-    if (( $# == 0 ))
-    then
-        git log --decorate --graph --oneline -n 10
-    else
-        git log --decorate --graph --oneline -n $1
-    fi
+  if (( $# == 0 )) then
+    git log --decorate --graph --oneline -n 10
+  else
+    git log --decorate --graph --oneline -n $1
+  fi
 }
 
 fnGitLogVerbose() {
-    if (( $# == 0 ))
-    then
-        git log --decorate --graph -n 10
-    else
-        git log --decorate --graph -n $1
-    fi
+  if (( $# == 0 )) then
+    git log --decorate --graph -n 10
+  else
+    git log --decorate --graph -n $1
+  fi
 }
