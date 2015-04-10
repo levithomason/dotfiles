7a59fae07a75622e0ad205a431c1c9e3148dcbb9
Levi Thomason <me@levithomason.com>
2015-04-10 16:46:56 -0700

    update git add

--- system/ssh.zsh
+++ system/ssh.zsh
@@ -1,5 +1,6 @@
 # Add all identities to the ssh agent
 
-for key in $(ls ~/.ssh/ | grep identity | grep -v pub); do
-  ssh-add ~/.ssh/$key;
-done
+echo "Skipping ssh-add, you should setup ~/.ssh/config magic instead"
+# for key in $(ls ~/.ssh/ | grep identity | grep -v pub); do
+#   ssh-add ~/.ssh/$key;
+# done
